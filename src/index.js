0f465bd6fb18a590e7302d029054a239731c5f64
Naimat Oyewale <naimatoyewale@gmail.com>
2021-06-23 13:04:11 +0100

    fix(dependencies): fix uninstalled dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import ussdRoute from './route/index';
 import { logger } from './config/loggerConfig';
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use("/router/digitalpulse/v1.0/ussd/", ussdRoute);
 
 
-const PORT = "";
+const PORT = 5000;
 
 app.listen(PORT, () => {
     logger.info(`App running on port ${PORT}`);
--- src/route/index.js
+++ src/route/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ussdController from '../controller/ussdIntegration';
 
 const ussdRoute = express.Router();
+const appId = "75442486-0878-440c-9db1-a7006c25a39f";
 
 ussdRoute.get(`reply/${appId}`, ussdController.clientIntegration);
 
